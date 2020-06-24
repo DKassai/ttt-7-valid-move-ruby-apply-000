aa1dce218472e864870c934b2c1356422c547f53
Dov Kassai <dovi.kassai@gmail.com>
2020-06-24 17:30:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,7 +11,7 @@ def position_taken?(board,index)
 end  
 
 def valid_move? (board,int)
-  if position_taken? && int.between(0,8)
+  if position_taken?(board,int) && int.between(0,8)
     true 
   else 
     false 
